7d1f269df4692063023bfc50fa43e9f5b3696ff1
Achyuth_Kumar <achyuthkumar0010@gmail.com>
2025-04-16 10:51:10 -0400

    committing another repo

---
+++ sessionsOnJava/src/main/java/packageOnsessionsOnJava/firstProgram.java
@@ -0,0 +1,20 @@
+package packageOnsessionsOnJava;
+
+public class firstProgram {
+	public static void srinivas() {
+		System.out.println("Achyuth");
+		
+	}
+	public static void main(String[] args) {
+		manoj();
+		System.out.println("Srinivas"); 
+		srinivas();
+		
+		
+	}
+	
+	public static void manoj() {
+		System.out.println("manoj");
+	}
+
+}
